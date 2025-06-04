e5a1d8cb318ea6375f282e90385ce2bb0c536143
Muhammed Sabir <sabircottage225@gmail.com>
2025-06-05 00:21:55 +0530

    Configured Tailwind for production

--- journal/urls.py
+++ journal/urls.py
@@ -2,6 +2,7 @@ from .views import fetch_and_store_question
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('fetch-question/', fetch_and_store_question, name='fetch_question'),
     path("", views.home_view, name="home"),
@@ -12,6 +13,7 @@ urlpatterns = [
     path('calendar-data/', views.calendar_data, name='calendar_data'),
     path('revision/' , views.revision_question , name='revision'),
     path("search/", views.live_search, name="live_search"),
+  
 
 
 ]
